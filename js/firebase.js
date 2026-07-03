b807299e0d81b081a54912b9cfaa43d036c6dd10
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-03 18:42:24 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,25 +1,37 @@
-// ==========================================================
-// KMYTE Online
-// Firebase Configuration
-// ==========================================================
+/* ==========================================================
+   KMYTE Online
+   Firebase Configuration
+========================================================== */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// Firebase Config
+/* ==========================================================
+   FIREBASE CONFIG
+========================================================== */
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyBUeWih6FR4brqc8mmNgqCv7blhN2s-t4w",
+
     authDomain: "kmyteonline.firebaseapp.com",
+
     projectId: "kmyteonline",
+
     storageBucket: "kmyteonline.firebasestorage.app",
+
     messagingSenderId: "732800529865",
+
     appId: "1:732800529865:web:660adf6e9288243bf8a418",
+
     measurementId: "G-E41CL2H01B"
+
 };
 
-// Initialize Firebase
+/* ==========================================================
+   INITIALIZE FIREBASE
+========================================================== */
 
 const app = initializeApp(firebaseConfig);
 
@@ -27,6 +39,8 @@ const auth = getAuth(app);
 
 const db = getFirestore(app);
 
-// Export
+/* ==========================================================
+   EXPORT SERVICES
+========================================================== */
 
 export { app, auth, db };
