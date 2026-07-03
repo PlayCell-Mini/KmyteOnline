5a40737d26b5aeef3e58732e512040af25e07645
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-04 02:01:21 +0500

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -4,14 +4,13 @@
 ========================================================== */
 
 import { auth, db } from "./firebase.js";
-
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import {
-
     doc,
     getDoc,
     setDoc,
+    updateDoc,
     serverTimestamp
-
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const setupBtn=document.getElementById("setupBtn");
