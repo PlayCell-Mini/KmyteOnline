087a550654dec4e057cbe01c11e2abb7c97570ee
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-03 18:38:27 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,32 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// ==========================================================
+// KMYTE Online
+// Firebase Configuration
+// ==========================================================
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+// Firebase Config
+
 const firebaseConfig = {
-apiKey: "AIzaSyBUeWih6FR4brqc8mmNgqCv7blhN2s-t4w",
-authDomain: "kmyteonline.firebaseapp.com",
-projectId: "kmyteonline",
-storageBucket: "kmyteonline.firebasestorage.app",
-messagingSenderId: "732800529865",
-appId: "1:732800529865:web:660adf6e9288243bf8a418",
-measurementId: "G-E41CL2H01B"
+    apiKey: "AIzaSyBUeWih6FR4brqc8mmNgqCv7blhN2s-t4w",
+    authDomain: "kmyteonline.firebaseapp.com",
+    projectId: "kmyteonline",
+    storageBucket: "kmyteonline.firebasestorage.app",
+    messagingSenderId: "732800529865",
+    appId: "1:732800529865:web:660adf6e9288243bf8a418",
+    measurementId: "G-E41CL2H01B"
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+// Export
+
+export { app, auth, db };
