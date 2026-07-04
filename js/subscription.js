d42209a8d788bb414275ffbfbf9e956d047137de
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-04 10:25:21 +0500

    Update subscription.js

---
+++ js/subscription.js
@@ -0,0 +1,40 @@
+/* ==========================================================
+   KMYTE Online
+   Subscription Module
+========================================================== */
+
+import { db } from "./firebase.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+async function loadPlans(){
+
+    const settingsRef = doc(db, "settings", "app");
+
+    const settingsSnap = await getDoc(settingsRef);
+
+    if(!settingsSnap.exists()){
+
+        alert("Settings not found.");
+
+        return;
+
+    }
+
+    const settings = settingsSnap.data();
+
+    document.getElementById("plan7Amount").textContent =
+        `PKR ${settings.plan7Amount}`;
+
+    document.getElementById("plan15Amount").textContent =
+        `PKR ${settings.plan15Amount}`;
+
+    document.getElementById("plan30Amount").textContent =
+        `PKR ${settings.plan30Amount}`;
+
+}
+
+loadPlans();
