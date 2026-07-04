256d0b3f508900d6db06682a608e793e5527441a
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-04 13:58:33 +0500

    Update paymentSubmit.js

--- js/paymentSubmit.js
+++ js/paymentSubmit.js
@@ -1,8 +1,55 @@
 /* ==========================================================
    KMYTE Online
    Payment Submit
+   Version : 1.0.0
 ========================================================== */
 
 import { auth, db, storage } from "./firebase.js";
 
-console.log("Payment Module Loaded");
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+/* ==========================================================
+   GET PAYMENT ID
+========================================================== */
+
+const params = new URLSearchParams(window.location.search);
+
+const paymentId = params.get("id");
+
+if (!paymentId) {
+
+    alert("Invalid Payment.");
+
+    window.location.href = "dashboard.html";
+
+}
+
+/* ==========================================================
+   LOAD PAYMENT
+========================================================== */
+
+async function loadPayment(){
+
+    const paymentRef = doc(db,"payments",paymentId);
+
+    const paymentSnap = await getDoc(paymentRef);
+
+    if(!paymentSnap.exists()){
+
+        alert("Payment not found.");
+
+        return;
+
+    }
+
+    const payment = paymentSnap.data();
+
+    document.getElementById("paymentAmount").textContent =
+        `Today's Payment : PKR ${payment.amount}`;
+
+}
+
+loadPayment();
