e4d9532c6c9c2d3e1d0d349163aefdb08ce3d8d4
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-04 13:45:39 +0500

    Create paymentSubmit.js

---
+++ js/paymentSubmit.js
@@ -0,0 +1,8 @@
+/* ==========================================================
+   KMYTE Online
+   Payment Submit
+========================================================== */
+
+import { auth, db, storage } from "./firebase.js";
+
+console.log("Payment Module Loaded");
