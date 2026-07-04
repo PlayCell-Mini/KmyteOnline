94c8e28942fe5a5cbf2e44880a0b9255450344fb
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-04 13:35:59 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,6 +6,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 
 /* ==========================================================
    FIREBASE CONFIG
