efc4f8ce0ccbd91b6265329a02e354ee47cb5b6d
PlayCell-Mini <chachaji4182@gmail.com>
2026-07-05 22:26:21 +0500

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -255,3 +255,42 @@ async function changePassword(e) {
     }
 
 }
+
+
+
+
+/* ==========================================================
+   SHOW / HIDE PASSWORD
+========================================================== */
+
+document.querySelectorAll(".toggle-password").forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const input = document.getElementById(button.dataset.target);
+
+        const icon = button.querySelector("i");
+
+        if (input.type === "password") {
+
+            input.type = "text";
+
+            icon.classList.remove("fa-eye");
+
+            icon.classList.add("fa-eye-slash");
+
+        }
+
+        else {
+
+            input.type = "password";
+
+            icon.classList.remove("fa-eye-slash");
+
+            icon.classList.add("fa-eye");
+
+        }
+
+    });
+
+});
